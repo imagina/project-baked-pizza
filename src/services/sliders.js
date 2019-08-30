593f1296a9d134a73e16bba5c94893631d1b7aad
Yeison Tapia <yftapial@ut.edu.co>
2019-08-30 15:58:28 -0500

    + Add slider service

---
+++ src/services/sliders.js
@@ -0,0 +1,26 @@
+import {Cookies, LocalStorage} from 'quasar'
+import config from 'src/config/index'
+import http from "axios"
+import {remember} from '@imagina/qhelper/_plugins/remember'
+
+export default {
+  show(id, filter = {}) {
+    let key = JSON.stringify(id);
+    return new Promise((resolve, reject) => {
+      //remember.async(key, 3600 * 3, () => {
+      return http.get(config('apiRoutes.api.api_url') + '/slider/v1/sliders/' + id, {
+        params: {
+          filter
+        }
+        /* })*/
+      }).then(response => {
+        resolve(response.data);
+      })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+}
+
